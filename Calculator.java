98b28bee5a78e24bfd9cbbf7bfb89709a7a5462d
alieu <alieujallow93@gmail.com>
2017-01-23 21:50:06 +0000

    add

---
+++ Calculator.java
@@ -0,0 +1,11 @@
+public class Calculator
+{
+	public static void main (String[]args)
+	{
+			if(args[1].equalsIgnoreCase("+"))
+			{
+				System.out.println(Integer.parseInt(args[0])+Integer.parseInt(args[2]));
+
+			}
+	}
+}
